32765b192ca48809e18da5c34bd2a7a782329564
Ramekici <46330945+Ramekici@users.noreply.github.com>
2020-10-01 15:40:10 +0300

    build

---
+++ client/build/precache-manifest.d40c3b38977405bb64c6f82628158292.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c40361ce9bed2d6a098222af7a47151",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c33c33a85df6137c6a5",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "0fa9ec750f4646eebd80",
+    "url": "/static/css/main.7d53c94d.chunk.css"
+  },
+  {
+    "revision": "0c33c33a85df6137c6a5",
+    "url": "/static/js/2.8bbb4b86.chunk.js"
+  },
+  {
+    "revision": "c3cb654a3a7d27a576aed8d503b856d8",
+    "url": "/static/js/2.8bbb4b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fa9ec750f4646eebd80",
+    "url": "/static/js/main.ca446737.chunk.js"
+  },
+  {
+    "revision": "888bc94b47cc48c82f5a",
+    "url": "/static/js/runtime-main.d4d22746.js"
+  }
+]);
